fa162fe3da10ebcbf31bbaa00d846c50300fd03d
sarahgl <sarah.llebres@gmail.com>
2026-07-27 21:34:36 +0200

    unidad6_ejercicio2

--- U6/U6EJ2.js
+++ U6/U6EJ2.js
@@ -3,28 +3,43 @@
 /** 
   -- CASTELLANO
   -- EJERCICIO 2.6.2 ENUNCIADO: 
-	Dado en el valor de la constante denominada myJSON, un texto plano en formato JSON que contiene la descripción de un Array con tres objetos de la clase Car,
+  Dado en el valor de la constante denominada myJSON, un texto plano en formato JSON que contiene la descripción de un Array con tres objetos de la clase Car,
   vista en el ejercicio 2.5.2. Se nos pide:
-	- Definir en una constante denominada myCars un Array de objetos Car a partir de este texto.
-	- Obtener un nuevo Array en otra constante (a la que llamaremos lessThan25) todos los coches definidos en el Array anterior (myCars) 
+  - Definir en una constante denominada myCars un Array de objetos Car a partir de este texto.
+  - Obtener un nuevo Array en otra constante (a la que llamaremos lessThan25) todos los coches definidos en el Array anterior (myCars) 
   que tienen una antiguedad inferior a 25 años.
-	- Pasar este nuevo Array a un texto plano JSON en una nueva constante denominada newCarsJSON.
+  - Pasar este nuevo Array a un texto plano JSON en una nueva constante denominada newCarsJSON.
 	
   
   -- CATALÀ
   -- EXERCICI 2.6.2 ENUNCIAT:
-	Donat el valor de la constant anomenada myJSON, un text pla en format JSON que conté la descripció d'un Array amb tres objectes de la classe Car,
+  Donat el valor de la constant anomenada myJSON, un text pla en format JSON que conté la descripció d'un Array amb tres objectes de la classe Car,
   vista a l'exercici 2.5.2. Se'ns demana:
-	- Definir en una constant anomenada myCars un Array d'objectes Car a partir d'aquest text.
-	- Obtenir un nou Array en una altra constant (a la qual anomenarem lessThan25) tots els cotxes definits a l'Array anterior (myCars) 
+  - Definir en una constant anomenada myCars un Array d'objectes Car a partir d'aquest text.
+  - Obtenir un nou Array en una altra constant (a la qual anomenarem lessThan25) tots els cotxes definits a l'Array anterior (myCars) 
   que tenen una antiguitat inferior a 25 anys.
-	- Passar aquest nou Array a un text pla JSON en una nova constant anomenada newCarsJSON.
+  - Passar aquest nou Array a un text pla JSON en una nova constant anomenada newCarsJSON.
 */
 
-const myJSON= '[{"brand":"Ford","model":"Mustang Boss 429","displacement":7000,"horsePower":375,"year":1969},{"brand":"BMW","model":"520d","displacement":1980,"horsePower":136,"year":2002},{"brand":"Mercedes-Benz","model":"280 SLC","displacement":2746,"horsePower":136,"year":1979}]';
+const myJSON = '[{"brand":"Ford","model":"Mustang Boss 429","displacement":7000,"horsePower":375,"year":1969},{"brand":"BMW","model":"520d","displacement":1980,"horsePower":136,"year":2002},{"brand":"Mercedes-Benz","model":"280 SLC","displacement":2746,"horsePower":136,"year":1979}]';
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+class Car {
+  constructor(brand, model, displacement, horsePower, year) {
+    this.brand = brand;
+    this.model = model;
+    this.displacement = displacement;
+    this.horsePower = horsePower;
+    this.year = year;
+  }
+}
 
+const carsAsJson = JSON.parse(myJSON);
+const myCars = carsAsJson.map(c => new Car(c.brand, c.model, c.displacement, c.horsePower, c.year));
+
+const anyoLimite = new Date().getFullYear() - 25;
+const lessThan25 = myCars.filter(c => c.year > anyoLimite);
+const newCarsJSON = JSON.stringify(lessThan25);
 
 
 
