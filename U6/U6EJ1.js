b9d75499e36244666dd0afa1a2b44ac0dc38d201
sarahgl <sarah.llebres@gmail.com>
2026-07-27 19:37:31 +0200

    unidad6_ejercicio1

--- U6/U6EJ1.js
+++ U6/U6EJ1.js
@@ -3,44 +3,50 @@
 /** 
   -- CASTELLANO
   -- EJERCICIO 2.6.1 ENUNCIADO: 
-	- Define, en el valor de una constante denominada myJSON, un texto plano en formato JSON que contenga la descripción de un Array con al menos tres objetos Planet definidos mediante la clase dada. 
-	- Después convierte este texto a un Array de objetos Planet e introduce en un nuevo Array denominado planetsBTE todos los que resulten de pasar este Array al el método de clase biggerThanEarth.
+  - Define, en el valor de una constante denominada myJSON, un texto plano en formato JSON que contenga la descripción de un Array con al menos tres objetos Planet definidos mediante la clase dada. 
+  - Después convierte este texto a un Array de objetos Planet e introduce en un nuevo Array denominado planetsBTE todos los que resulten de pasar este Array al el método de clase biggerThanEarth.
   
   -- CATALÀ
   -- EXERCICI 2.6.1 ENUNCIAT:
-	- Defineix, en el valor d'una constant anomenada myJSON, un text pla en format JSON que contingui la descripció d'un Array amb almenys tres objectes Planet definits mitjançant la classe donada.
-	- Després converteix aquest text a un Array d'objectes Planet i introdueix en un nou Array anomenat planetsBTE tots els que resultin de passar aquest Array al mètode de classe biggerThanEarth.
+  - Defineix, en el valor d'una constant anomenada myJSON, un text pla en format JSON que contingui la descripció d'un Array amb almenys tres objectes Planet definits mitjançant la classe donada.
+  - Després converteix aquest text a un Array d'objectes Planet i introdueix en un nou Array anomenat planetsBTE tots els que resultin de passar aquest Array al mètode de classe biggerThanEarth.
 */
 
 class Planet {
-    constructor(p_name, p_distanceToSun, p_hasRings, p_diameter){
-		this.name = p_name;
-        this.distanceToSun = p_distanceToSun;
-        this.hasRings = p_hasRings;
-        this.diameter = p_diameter;
-    }
-	
-	//Class methods
-	static biggerThanEarth(planets){
-		let planetsArray = new Array;
-		planets.forEach(function(planet){
-            console.log(planet.diameter , Planet.earthDiameter())
-			if(planet.diameter > Planet.earthDiameter()){
-				planetsArray.push(planet);
-			} 
-		});	
-		return planetsArray;
-	}
-	//Class methods
-	static earthDiameter(){
-		return 12756;
-	}
+  constructor(p_name, p_distanceToSun, p_hasRings, p_diameter) {
+    this.name = p_name;
+    this.distanceToSun = p_distanceToSun;
+    this.hasRings = p_hasRings;
+    this.diameter = p_diameter;
+  }
+
+  //Class methods
+  static biggerThanEarth(planets) {
+    let planetsArray = new Array;
+    planets.forEach(function (planet) {
+      console.log(planet.diameter, Planet.earthDiameter())
+      if (planet.diameter > Planet.earthDiameter()) {
+        planetsArray.push(planet);
+      }
+    });
+    return planetsArray;
+  }
+  //Class methods
+  static earthDiameter() {
+    return 12756;
+  }
 }
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+const myJSON = `[
+  { "name": "Mercury", "distanceToSun": 58344000, "hasRings": false, "diameter": 4878 },
+  { "name": "Venus", "distanceToSun": 107712000, "hasRings": false, "diameter": 12100 },
+  { "name": "Earth", "distanceToSun": 149600000, "hasRings": false, "diameter": 12756 }
+]`;
 
-
-
+const planetsAsJson = JSON.parse(myJSON);
+const planetsAsArray = planetsAsJson.map(p => new Planet(p.name, p.distanceToSun, p.hasRings, p.diameter));
+const planetsBTE = Planet.biggerThanEarth(planetsAsArray);
 
 /**
  * TEST
@@ -49,5 +55,5 @@ class Planet {
  * please comment or remove the following lines
  */
 export function tests() {
-    return [Planet, myJSON, planetsBTE];
+  return [Planet, myJSON, planetsBTE];
 }
